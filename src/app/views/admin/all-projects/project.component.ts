7746ff2659ddf39fb336db37f03e15758e0c11ca
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2018-11-19 15:49:32 -0500

    project form bug fix

--- src/app/views/admin/all-projects/project.component.ts
+++ src/app/views/admin/all-projects/project.component.ts
@@ -423,7 +423,7 @@ export class ProjectComponent implements OnInit {
   diff_years(dt2, dt1) {
     let diff = (dt2.getTime() - dt1.getTime()) / 1000;
     diff /= (60 * 60 * 24);
-    return Math.abs(Math.round(diff / 365.25));
+    return Math.abs(Math.ceil(diff / 365.25));
   }
 
   removeP() {
